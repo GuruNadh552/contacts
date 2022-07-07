885704aa07a0a609607b3ef66cbbf843b53f4211
Your Name <you@example.com>
2022-07-07 15:28:21 +0530

    added contacts sub app

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,5 +1,5 @@
 import { ContactsComponent } from './contacts.component';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -13,7 +13,8 @@ const routes : Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ContactsModule { }
